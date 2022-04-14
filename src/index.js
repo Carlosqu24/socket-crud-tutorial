a34cadd0f16ef8347e9994ac907c64768da29399
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-04-14 05:16:31 -0600

    Socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 const app = express();
+import path from 'path';
 
-app.set('port', process.env.PORT || 3500)
+import { Server as WebsocketServer } from 'socket.io';
+import http from 'http';
 
 
+app.set('port', process.env.PORT || 3500);
 
-app.listen(app.get('port'), () => {
-      console.log("http://localhost:" + app.get('port'))
-})
+app.use(express.static(path.join(__dirname + 'public')));
+
+const server = http.createServer(app);
+const httpServer = server.listen(app.get('port'));
+const io = new WebsocketServer(httpServer);
+
+console.log('http://localhost:' + app.get('port'));
